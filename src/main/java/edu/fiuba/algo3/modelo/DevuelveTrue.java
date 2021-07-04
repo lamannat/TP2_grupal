d470f13cf61896d8c0067a54a7470500d217c5a6
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-04 16:40:11 -0300

    Creando una clase y un test trivial

---
+++ src/main/java/edu/fiuba/algo3/modelo/DevuelveTrue.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo;
+
+public class DevuelveTrue {
+    public boolean devuelveTrue() {
+        return true;
+    }
+}
---
+++ src/main/test/edu/fiuba/algo3/modelo/DevuelveTrueTest.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DevuelveTrueTest {
+    @Test
+    public void TesteoDevuelveTrue(){
+        DevuelveTrue x = new DevuelveTrue();
+        boolean y = x.devuelveTrue();
+        assertEquals(y, true);
+    }
+
+}
