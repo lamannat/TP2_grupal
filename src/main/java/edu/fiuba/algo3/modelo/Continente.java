7d5db8deade7431f44e051316f0a6f8c40dd610b
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-11 16:32:29 -0300

    Creando las clases Pais y Continente

---
+++ src/main/java/edu/fiuba/algo3/modelo/Continente.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.List;
+
+public class Continente {
+
+    private List<Pais> paises;
+
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -0,0 +1,16 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.List;
+
+public class Pais {
+
+    private final String nombre;
+    private List<Pais> limitrofes;
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+}
