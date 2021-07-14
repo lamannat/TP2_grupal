fc762258e034557b0c475966e0c58a58516ec1a8
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-13 22:17:08 -0300

    Cambiando la forma en al que se hace las batallas, ya que tiene ejercitos y no paises, y la se asume que la batalla de por si es valida

--- src/main/java/edu/fiuba/algo3/modelo/Batalla.java
+++ src/main/java/edu/fiuba/algo3/modelo/Batalla.java
@@ -3,10 +3,10 @@ package edu.fiuba.algo3.modelo;
 import java.util.List;
 
 public class Batalla {
-    private final Pais defensor;
-    private final Pais atacante;
+    private final Ejercitos defensor;
+    private final Ejercitos atacante;
 
-    public Batalla(Pais defensor, Pais atacante) {
+    public Batalla(Ejercitos defensor, Ejercitos atacante) {
         this.defensor = defensor;
         this.atacante = atacante;
     }
@@ -14,34 +14,15 @@ public class Batalla {
     public void comenzarBatalla(Dado dado) {
         List<Integer> dadosAtaque, dadosDefensa;
 
-        if (!batallaValida())
-            return;
-
-        Ejercitos ejercitoAtacante = atacante.atacoConEjercito();
-        Ejercitos ejercitoDefensor = defensor.atacoConEjercito();
-
-        dadosAtaque = dado.tirarDados(ejercitoAtacante.ejercitosParaAtaque());
-        dadosDefensa = dado.tirarDados(ejercitoDefensor.ejercitosParaDefensa());
+        dadosAtaque = dado.tirarDados(atacante.ejercitosParaAtaque());
+        dadosDefensa = dado.tirarDados(defensor.ejercitosParaDefensa());
 
         int cantidadDeAtaques = Math.min(dadosAtaque.size(), dadosDefensa.size());
         for (int i = 0; i < cantidadDeAtaques; i++){
             if(dadosAtaque.get(i) > dadosDefensa.get(i))
-                ejercitoDefensor.pierdeEjercitos(1);
+                defensor.pierdeContraEjercito(atacante);
             else
-                ejercitoAtacante.pierdeEjercitos(1);
+                atacante.pierdeContraEjercito(defensor);
         }
-
-        if (ejercitoDefensor.perdioBatalla())
-            this.atacante.conquista(this.defensor);
-//            this.defensor.conquistadoPor(this.atacante);
     }
-
-    private boolean batallaValida() {
-        boolean atacantePuedeLuchar = this.atacante.puedeAtacar();
-        boolean sonLimitrofes = this.atacante.tienePaisLimitrofe(this.defensor);
-        boolean esAliado = this.atacante.esAliado(this.defensor);
-
-        return atacantePuedeLuchar && sonLimitrofes && !esAliado;
-    }
-
 }
--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -34,20 +34,35 @@ public class Pais {
         return this.ejercitos.puedeAtacar();
     }
 
-    public boolean perdioBatalla() {
-        return this.ejercitos.perdioBatalla();
-    }
-
-    public void conquista(Pais conquistado) {
-        Ejercitos ejercitoConquistado = conquistado.atacoConEjercito();
-        this.ejercitos.conquista(ejercitoConquistado);
-    }
+//    public void conquista(Pais conquistado) {
+//        Ejercitos ejercitoConquistado = conquistado.atacoConEjercito();
+//        this.ejercitos.conquista(ejercitoConquistado);
+//    }
 
     public boolean tienePaisLimitrofe(Pais pais) {
         return this.limitrofes.esLimitrofe(pais);
     }
 
     public boolean tieneColor(Colores unColor) { return this.ejercitos.tieneColor(unColor); }
+
+    public void atacaPais(Pais pais, Dado dado) {
+        if (!puedeAtacarAPais(pais))
+            return;
+
+        Ejercitos ejercitoDefensor = pais.atacoConEjercito();
+        Ejercitos ejercitoAtacante = this.ejercitos;
+
+        Batalla batalla = new Batalla(ejercitoDefensor, ejercitoAtacante);
+        batalla.comenzarBatalla(dado);
+    }
+
+    private boolean puedeAtacarAPais(Pais pais) {
+        boolean puedeAtacar = this.ejercitos.puedeAtacar();
+        boolean esLimitrofe = this.limitrofes.esLimitrofe(pais);
+        boolean esAleado = esAliado(pais);
+
+        return puedeAtacar && esLimitrofe && !esAleado;
+    }
 
     public boolean esAliado(Pais pais){
         return pais.tieneColor(this.ejercitos.getColor());
