41734f8ed4d325d64b50abaff852b392ffec7074
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-14 18:08:14 -0300

    Creando la forma de leer el archivo, creando una lista de listas de strings

--- src/main/java/edu/fiuba/algo3/modelo/LeerArchivo.java
+++ src/main/java/edu/fiuba/algo3/modelo/LeerArchivo.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeerArchivo {
@@ -17,13 +18,8 @@ public class LeerArchivo {
             String cadena;
             FileReader f = new FileReader(archivo);
             BufferedReader b = new BufferedReader(f);
-            while((cadena = b.readLine())!=null) {
-
-                System.out.println(cadena);
-
-
-
-            }
+            while((cadena = b.readLine())!=null)
+                resultado.add(Arrays.asList(cadena.split(",", 0)));
             b.close();
         } catch (FileNotFoundException f) {
             System.out.println("El archivo no se encontro");
