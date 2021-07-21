2d339f1764fc33e1a253786e29a094c63c982f02
V-SANT <vsantander@fi.uba.ar>
2021-07-21 11:52:40 -0300

    Se creó la clase Ficha

--- src/main/java/edu/fiuba/algo3/modelo/Ejercitos.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ejercitos.java
@@ -1,36 +1,49 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Ejercitos {
     private Colores color;
-    public int ejercitos;
+    public List<Ficha> fichas;
     private final static int cantidadMaximaDeDados = 3;
 
     public Ejercitos() {
-        this.ejercitos = 1;
         this.color = Colores.NO_ASIGNADO;
+        this.fichas = new ArrayList<>();
+        Ficha ficha = new Ficha(Colores.NO_ASIGNADO);
+        fichas.add(ficha);
     }
-
 
     public void setColor(Colores color) {
         this.color = color;
+        this.fichas.forEach(ficha -> { ficha.setColor(color);});
+    }
+
+    public Colores getColor(){
+        return this.color;
     }
 
     public void agregarEjercitos(int cantidad) {
-        this.ejercitos += cantidad;
+        for (int i = 0; i < cantidad; i++) {
+            Ficha ficha = new Ficha(this.color);
+            this.fichas.add(ficha);
+        }
     }
 
     public int ejercitosParaAtaque() {
-        return Math.min(this.ejercitos - 1, cantidadMaximaDeDados);
+        return Math.min(this.fichas.size() - 1, cantidadMaximaDeDados);
     }
 
     public int ejercitosParaDefensa() {
-        return Math.min(this.ejercitos, cantidadMaximaDeDados);
+        return Math.min(this.fichas.size(), cantidadMaximaDeDados);
     }
 
     public boolean tieneColor(Colores unColor) { return this.color.equals(unColor); }
 
     public boolean puedeAtacar() {
-        return this.ejercitos > 1;
+        return this.fichas.size() > 1;
     }
 
     public void conquista(Ejercitos conquistado) {
@@ -39,18 +52,14 @@ public class Ejercitos {
     }
 
     public void moverTropa(Ejercitos destino) {
-        this.ejercitos -= 1;
+        this.fichas.remove(0);
         destino.agregarEjercitos(1);
     }
 
     public void pierdeContraEjercito(Ejercitos conquistador) {
-        this.ejercitos -= 1;
-        if (this.ejercitos < 1)
+        this.fichas.remove(0);
+        if (this.fichas.size() < 1)
             conquistador.conquista(this);
-    }
-
-    public Colores getColor(){
-        return this.color;
     }
 
     public boolean esAliado(Ejercitos unEjercito){
---
+++ src/main/java/edu/fiuba/algo3/modelo/Ficha.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.modelo;
+
+public class Ficha {
+    private Colores color;
+
+    public Ficha(Colores color) {
+        this.color = color;
+    }
+
+    public Colores getColor() {
+        return color;
+    }
+
+    public void setColor(Colores color) {
+        this.color = color;
+    }
+}
