bb63d1c36bd35269069a70be25020cc04bc0af0c
Dolores Levi <dolevi@fi.uba.ar>
2021-07-22 10:03:21 -0300

    eliminamos codigo comentado en EjercitoDeBatalla

--- src/main/java/edu/fiuba/algo3/modelo/Batalla.java
+++ src/main/java/edu/fiuba/algo3/modelo/Batalla.java
@@ -5,10 +5,6 @@ import java.util.List;
 public class Batalla {
 
     static void ejercitoAtacaAEjercito(EjercitoDeBatalla atacante, EjercitoDeBatalla defensor, Dado unDado) {
-//        List<Integer> dadosAtaque, dadosDefensa;
-
-//        dadosAtaque = TiradaDeDados.tirarDadosDeClase(atacante.cantidadDeFichas());
-//        dadosDefensa = TiradaDeDados.tirarDadosDeClase(defensor.cantidadDeFichas());
 
         TiradaDeDados tiradaAtacante = unDado.tirarDado(atacante.cantidadDeFichas());
         TiradaDeDados tiradaDefensor = unDado.tirarDado(defensor.cantidadDeFichas());
@@ -20,14 +16,5 @@ public class Batalla {
                 atacante.pierdeFicha();
 
         }
-
-
-//        int cantidadDeAtaques = Math.min(dadosAtaque.size(), dadosDefensa.size());
-//        for (int i = 0; i < cantidadDeAtaques; i++){
-//            if(dadosAtaque.get(i) > dadosDefensa.get(i))
-//                defensor.pierdeFicha();
-//            else
-//                atacante.pierdeFicha();
-//        }
     }
 }
