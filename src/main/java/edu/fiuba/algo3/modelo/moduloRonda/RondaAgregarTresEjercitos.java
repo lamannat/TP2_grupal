43f4e4b095df43eb0b137965a251c6c48c459788
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-22 17:19:57 -0300

    Agregando la clase RondaAgregarEjercito para hacer una clase generica de agregar fichas

--- src/main/java/edu/fiuba/algo3/modelo/moduloRonda/RondaAgregarTresEjercitos.java
+++ src/main/java/edu/fiuba/algo3/modelo/moduloRonda/RondaAgregarTresEjercitos.java
@@ -3,16 +3,11 @@ package edu.fiuba.algo3.modelo.moduloRonda;
 import edu.fiuba.algo3.modelo.Juego;
 import edu.fiuba.algo3.modelo.Jugador;
 
-public class RondaAgregarTresEjercitos implements Ronda {
-    private Juego juego;
-    private static int cantFichas = 3;
+public class RondaAgregarTresEjercitos extends RondaAgregarEjercitos {
 
     public RondaAgregarTresEjercitos(Juego juego) {
         this.juego = juego;
-    }
-
-    public void comenzarLaRonda(Jugador jugador){
-        this.juego.darleFichasAJugador(jugador,cantFichas);
+        cantFichas = 3;
     }
 
     public Ronda siguienteRonda(){
