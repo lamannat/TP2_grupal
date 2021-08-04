6621242027dc208c19bc0842cca1954a81617e74
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-08-04 11:44:35 -0300

    No se borre cosas, averigualo

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -20,34 +20,10 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     Stage ventana;
-    Scene sceneTitulo, sceneCantJugadores, sceneNombresColores;
-    Button botonVolver;
 
     @Override
     public void start(Stage stage) {
         ventana = stage;
-
-//        // BOTON VOLVER
-//        botonVolver = new Button("VOLVER");
-//        botonVolver.setOnAction(e -> ventana.setScene(sceneTitulo));
-//        botonVolver.setAlignment(Pos.CENTER);
-//        botonVolver.setScaleX(2);
-//        botonVolver.setScaleY(2);
-//        botonVolver.setStyle("-fx-background-color: black");
-//        botonVolver.setTextFill(Color.WHITE);
-//
-//        BorderPane pane = new BorderPane();
-//        pane.setCenter(botonVolver);
-//
-//        Scene sceneVolver = new Scene(pane);
-
-//        VistaSetearNombresYColores nombresYColores = new VistaSetearNombresYColores();
-//        Scene sceneNombresColores =  nombresYColores.crearJugadores(ventana,sceneVolver);
-//
-//        VistaSetearCantidadJugadores cantJugadores = new VistaSetearCantidadJugadores();
-//        Scene sceneCantJugadores = cantJugadores.crearCantJugadores(ventana,sceneNombresColores);
-
-//        VBox padre = new VBox();
 
         SetUpJuego inicio = new SetUpJuego();
 
